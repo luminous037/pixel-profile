edfbf9b1d329569ee9dfda892ecee3ee2e5d8c6e
Jiji <luminous037@gmail.com>
2025-06-07 15:02:56 +0900

    add test port num pixel-profile-server/src/utils/node.ts for test

--- packages/pixel-profile-server/src/node.ts
+++ packages/pixel-profile-server/src/node.ts
@@ -2,11 +2,12 @@ import 'dotenv/config'
 import { app } from './app'
 import { serve } from '@hono/node-server'
 
-const PORT = Number(process.env.PORT) || 3000
+const PORT = Number(process.env.PORT) || 10277
 
 console.log(`The server is start, Port is ${PORT}`)
 
 serve({
   fetch: app.fetch,
-  port: PORT
+  port: PORT,
+  hostname: '0.0.0.0'
 })
